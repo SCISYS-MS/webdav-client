6aaf9fba53e4ec2aba857833b20d8e034ac0aa73
munirdalkaya <35899018+munirdalkaya@users.noreply.github.com>
2018-12-18 00:36:19 +0100

    added getDirectoryContentsRecursive for recursive retrival

--- source/factory.js
+++ source/factory.js
@@ -1,6 +1,12 @@
+"use strict";
+
 const authTools = require("./auth.js");
 const urlTools = require("./url.js");
-const { merge } = require("./merge.js");
+
+var _require = require("./merge.js");
+
+const merge = _require.merge;
+
 
 const directoryContents = require("./interface/directoryContents.js");
 const createDir = require("./interface/createDirectory.js");
@@ -80,213 +86,214 @@ const stats = require("./interface/stat.js");
  *      });
  */
 function createClient(remoteURL, { username, password, httpAgent, httpsAgent, token = null } = {}) {
-    const baseOptions = {
-        headers: {},
-        remotePath: urlTools.extractURLPath(remoteURL),
-        remoteURL,
-        httpAgent,
-        httpsAgent
-    };
-    // Configure auth
-    if (username) {
-        baseOptions.headers.Authorization = authTools.generateBasicAuthHeader(username, password);
-    } else if (token && typeof token === "object") {
-        baseOptions.headers.Authorization = authTools.generateTokenAuthHeader(token);
+  const baseOptions = {
+    headers: {},
+    remotePath: urlTools.extractURLPath(remoteURL),
+    remoteURL,
+    httpAgent,
+    httpsAgent
+  };
+  // Configure auth
+  if (username) {
+    baseOptions.headers.Authorization = authTools.generateBasicAuthHeader(username, password);
+  } else if (token && typeof token === "object") {
+    baseOptions.headers.Authorization = authTools.generateTokenAuthHeader(token);
+  }
+  return {
+    /**
+     * Copy a remote item to another path
+     * @param {String} remotePath The remote item path
+     * @param {String} targetRemotePath The path file will be copied to
+     * @param {UserOptions=} options Options for the request
+     * @memberof ClientInterface
+     * @returns {Promise} A promise that resolves once the request has completed
+     * @example
+     *      await client.copyFile("/photos/pic1.jpg", "/backup/pic1.jpg");
+     */
+    copyFile: function copyFile(remotePath, targetRemotePath, options) {
+      const copyOptions = merge(baseOptions, options || {});
+      return copy.copyFile(remotePath, targetRemotePath, copyOptions);
+    },
+
+    /**
+     * Create a directory
+     * @param {String} dirPath The path to create
+     * @param {UserOptions=} options Options for the request
+     * @memberof ClientInterface
+     * @returns {Promise} A promise that resolves when the remote path has been created
+     * @example
+     *      await client.createDirectory("/my/directory");
+     */
+    createDirectory: function createDirectory(dirPath, options) {
+      const createOptions = merge(baseOptions, options || {});
+      return createDir.createDirectory(dirPath, createOptions);
+    },
+
+    /**
+     * Create a readable stream of a remote file
+     * @param {String} remoteFilename The file to stream
+     * @param {UserOptions=} options Options for the request
+     * @memberof ClientInterface
+     * @returns {Readable} A readable stream
+     * @example
+     *      const remote = client.createReadStream("/data.zip");
+     *      remote.pipe(someWriteStream);
+     */
+    createReadStream: function createReadStream(remoteFilename, options) {
+      const createOptions = merge(baseOptions, options || {});
+      return createStream.createReadStream(remoteFilename, createOptions);
+    },
+
+    /**
+     * Create a writeable stream to a remote file
+     * @param {String} remoteFilename The file to write to
+     * @param {PutOptions=} options Options for the request
+     * @memberof ClientInterface
+     * @returns {Writeable} A writeable stream
+     * @example
+     *      const remote = client.createWriteStream("/data.zip");
+     *      fs.createReadStream("~/myData.zip").pipe(remote);
+     */
+    createWriteStream: function createWriteStream(remoteFilename, options) {
+      const createOptions = merge(baseOptions, options || {});
+      return createStream.createWriteStream(remoteFilename, createOptions);
+    },
+
+    /**
+     * Delete a remote file
+     * @param {String} remotePath The remote path to delete
+     * @param {UserOptions=} options The options for the request
+     * @memberof ClientInterface
+     * @returns {Promise} A promise that resolves when the remote file as been deleted
+     * @example
+     *      await client.deleteFile("/some/file.txt");
+     */
+    deleteFile: function deleteFile(remotePath, options) {
+      const deleteOptions = merge(baseOptions, options || {});
+      return deletion.deleteFile(remotePath, deleteOptions);
+    },
+
+    /**
+     * Get the contents of a remote directory
+     * @param {String} remotePath The path to fetch the contents of
+     * @param {OptionsForAdvancedResponses=} options Options for the remote the request
+     * @returns {Promise.<Array>} A promise that resolves with an array of remote item stats
+     * @memberof ClientInterface
+     * @example
+     *      const contents = await client.getDirectoryContents("/");
+     */
+    getDirectoryContents: function getDirectoryContents(remotePath, options) {
+      const getOptions = merge(baseOptions, options || {});
+      return directoryContents.getDirectoryContents(remotePath, getOptions);
+    },
+    getDirectoryContentsRecursive: function getDirectoryContentsRecursive(remotePath, options) {
+      const getOptions = merge(baseOptions, options || {});
+      return directoryContents.getDirectoryContentsRecursive(remotePath, getOptions);
+    },
+    /**
+     * Get the contents of a remote file
+     * @param {String} remoteFilename The file to fetch
+     * @param {OptionsWithFormat=} options Options for the request
+     * @memberof ClientInterface
+     * @returns {Promise.<Buffer|String>} A promise that resolves with the contents of the remote file
+     * @example
+     *      // Fetching data:
+     *      const buff = await client.getFileContents("/image.png");
+     *      // Fetching text:
+     *      const txt = await client.getFileContents("/list.txt", { format: "text" });
+     */
+    getFileContents: function getFileContents(remoteFilename, options) {
+      const getOptions = merge(baseOptions, options || {});
+      getOptions.format = getOptions.format || "binary";
+      if (["binary", "text"].indexOf(getOptions.format) < 0) {
+        throw new Error("Unknown format: " + getOptions.format);
+      }
+      return getOptions.format === "text" ? getFile.getFileContentsString(remoteFilename, getOptions) : getFile.getFileContentsBuffer(remoteFilename, getOptions);
+    },
+
+    /**
+     * Get the download link of a remote file
+     * Only supported for Basic authentication or unauthenticated connections.
+     * @param {String} remoteFilename The file url to fetch
+     * @param {UserOptions=} options Options for the request
+     * @memberof ClientInterface
+     * @returns {String} A download URL
+     */
+    getFileDownloadLink: function getFileDownloadLink(remoteFilename, options) {
+      const getOptions = merge(baseOptions, options || {});
+      return getFile.getFileLink(remoteFilename, getOptions);
+    },
+
+    /**
+     * Get quota information
+     * @param {OptionsForAdvancedResponses=} options Options for the request
+     * @returns {null|Object} Returns null if failed, or an object with `used` and `available`
+     * @memberof ClientInterface
+     */
+    getQuota: function getQuota(options) {
+      const getOptions = merge(baseOptions, options || {});
+      return quota.getQuota(getOptions);
+    },
+
+    /**
+     * Move a remote item to another path
+     * @param {String} remotePath The remote item path
+     * @param {String} targetRemotePath The new path after moving
+     * @param {UserOptions=} options Options for the request
+     * @memberof ClientInterface
+     * @returns {Promise} A promise that resolves once the request has completed
+     * @example
+     *      await client.moveFile("/sub/file.dat", "/another/dir/file.dat");
+     */
+    moveFile: function moveFile(remotePath, targetRemotePath, options) {
+      const moveOptions = merge(baseOptions, options || {});
+      return move.moveFile(remotePath, targetRemotePath, moveOptions);
+    },
+
+    /**
+     * Write contents to a remote file path
+     * @param {String} remoteFilename The path of the remote file
+     * @param {String|Buffer} data The data to write
+     * @param {PutOptions=} options The options for the request
+     * @returns {Promise} A promise that resolves once the contents have been written
+     * @memberof ClientInterface
+     * @example
+     *      await client.putFileContents("/dir/image.png", myImageBuffer);
+     *      // Put contents without overwriting:
+     *      await client.putFileContents("/dir/image.png", myImageBuffer, { overwrite: false });
+     */
+    putFileContents: function putFileContents(remoteFilename, data, options) {
+      const putOptions = merge(baseOptions, options || {});
+      return putFile.putFileContents(remoteFilename, data, putOptions);
+    },
+
+    /**
+     * Get the upload link
+     * Only supported for Basic authentication or unauthenticated connections.
+     * @param {String} remoteFilename The path of the remote file location
+     * @param {PutOptions=} options The options for the request
+     * @memberof ClientInterface
+     * @returns {String} A upload URL
+     */
+    getFileUploadLink: function getFileUploadLink(remoteFilename, options) {
+      var putOptions = merge(baseOptions, options || {});
+      return putFile.getFileUploadLink(remoteFilename, putOptions);
+    },
+
+    /**
+     * Stat a remote object
+     * @param {String} remotePath The path of the item
+     * @param {OptionsForAdvancedResponses=} options Options for the request
+     * @memberof ClientInterface
+     * @returns {Promise.<Object>} A promise that resolves with the stat data
+     */
+    stat: function stat(remotePath, options) {
+      const getOptions = merge(baseOptions, options || {});
+      return stats.getStat(remotePath, getOptions);
     }
-    return {
-        /**
-         * Copy a remote item to another path
-         * @param {String} remotePath The remote item path
-         * @param {String} targetRemotePath The path file will be copied to
-         * @param {UserOptions=} options Options for the request
-         * @memberof ClientInterface
-         * @returns {Promise} A promise that resolves once the request has completed
-         * @example
-         *      await client.copyFile("/photos/pic1.jpg", "/backup/pic1.jpg");
-         */
-        copyFile: function copyFile(remotePath, targetRemotePath, options) {
-            const copyOptions = merge(baseOptions, options || {});
-            return copy.copyFile(remotePath, targetRemotePath, copyOptions);
-        },
-
-        /**
-         * Create a directory
-         * @param {String} dirPath The path to create
-         * @param {UserOptions=} options Options for the request
-         * @memberof ClientInterface
-         * @returns {Promise} A promise that resolves when the remote path has been created
-         * @example
-         *      await client.createDirectory("/my/directory");
-         */
-        createDirectory: function createDirectory(dirPath, options) {
-            const createOptions = merge(baseOptions, options || {});
-            return createDir.createDirectory(dirPath, createOptions);
-        },
-
-        /**
-         * Create a readable stream of a remote file
-         * @param {String} remoteFilename The file to stream
-         * @param {UserOptions=} options Options for the request
-         * @memberof ClientInterface
-         * @returns {Readable} A readable stream
-         * @example
-         *      const remote = client.createReadStream("/data.zip");
-         *      remote.pipe(someWriteStream);
-         */
-        createReadStream: function createReadStream(remoteFilename, options) {
-            const createOptions = merge(baseOptions, options || {});
-            return createStream.createReadStream(remoteFilename, createOptions);
-        },
-
-        /**
-         * Create a writeable stream to a remote file
-         * @param {String} remoteFilename The file to write to
-         * @param {PutOptions=} options Options for the request
-         * @memberof ClientInterface
-         * @returns {Writeable} A writeable stream
-         * @example
-         *      const remote = client.createWriteStream("/data.zip");
-         *      fs.createReadStream("~/myData.zip").pipe(remote);
-         */
-        createWriteStream: function createWriteStream(remoteFilename, options) {
-            const createOptions = merge(baseOptions, options || {});
-            return createStream.createWriteStream(remoteFilename, createOptions);
-        },
-
-        /**
-         * Delete a remote file
-         * @param {String} remotePath The remote path to delete
-         * @param {UserOptions=} options The options for the request
-         * @memberof ClientInterface
-         * @returns {Promise} A promise that resolves when the remote file as been deleted
-         * @example
-         *      await client.deleteFile("/some/file.txt");
-         */
-        deleteFile: function deleteFile(remotePath, options) {
-            const deleteOptions = merge(baseOptions, options || {});
-            return deletion.deleteFile(remotePath, deleteOptions);
-        },
-
-        /**
-         * Get the contents of a remote directory
-         * @param {String} remotePath The path to fetch the contents of
-         * @param {OptionsForAdvancedResponses=} options Options for the remote the request
-         * @returns {Promise.<Array>} A promise that resolves with an array of remote item stats
-         * @memberof ClientInterface
-         * @example
-         *      const contents = await client.getDirectoryContents("/");
-         */
-        getDirectoryContents: function getDirectoryContents(remotePath, options) {
-            const getOptions = merge(baseOptions, options || {});
-            return directoryContents.getDirectoryContents(remotePath, getOptions);
-        },
-
-        /**
-         * Get the contents of a remote file
-         * @param {String} remoteFilename The file to fetch
-         * @param {OptionsWithFormat=} options Options for the request
-         * @memberof ClientInterface
-         * @returns {Promise.<Buffer|String>} A promise that resolves with the contents of the remote file
-         * @example
-         *      // Fetching data:
-         *      const buff = await client.getFileContents("/image.png");
-         *      // Fetching text:
-         *      const txt = await client.getFileContents("/list.txt", { format: "text" });
-         */
-        getFileContents: function getFileContents(remoteFilename, options) {
-            const getOptions = merge(baseOptions, options || {});
-            getOptions.format = getOptions.format || "binary";
-            if (["binary", "text"].indexOf(getOptions.format) < 0) {
-                throw new Error("Unknown format: " + getOptions.format);
-            }
-            return getOptions.format === "text"
-                ? getFile.getFileContentsString(remoteFilename, getOptions)
-                : getFile.getFileContentsBuffer(remoteFilename, getOptions);
-        },
-
-        /**
-         * Get the download link of a remote file
-         * Only supported for Basic authentication or unauthenticated connections.
-         * @param {String} remoteFilename The file url to fetch
-         * @param {UserOptions=} options Options for the request
-         * @memberof ClientInterface
-         * @returns {String} A download URL
-         */
-        getFileDownloadLink: function getFileDownloadLink(remoteFilename, options) {
-            const getOptions = merge(baseOptions, options || {});
-            return getFile.getFileLink(remoteFilename, getOptions);
-        },
-
-        /**
-         * Get quota information
-         * @param {OptionsForAdvancedResponses=} options Options for the request
-         * @returns {null|Object} Returns null if failed, or an object with `used` and `available`
-         * @memberof ClientInterface
-         */
-        getQuota: function getQuota(options) {
-            const getOptions = merge(baseOptions, options || {});
-            return quota.getQuota(getOptions);
-        },
-
-        /**
-         * Move a remote item to another path
-         * @param {String} remotePath The remote item path
-         * @param {String} targetRemotePath The new path after moving
-         * @param {UserOptions=} options Options for the request
-         * @memberof ClientInterface
-         * @returns {Promise} A promise that resolves once the request has completed
-         * @example
-         *      await client.moveFile("/sub/file.dat", "/another/dir/file.dat");
-         */
-        moveFile: function moveFile(remotePath, targetRemotePath, options) {
-            const moveOptions = merge(baseOptions, options || {});
-            return move.moveFile(remotePath, targetRemotePath, moveOptions);
-        },
-
-        /**
-         * Write contents to a remote file path
-         * @param {String} remoteFilename The path of the remote file
-         * @param {String|Buffer} data The data to write
-         * @param {PutOptions=} options The options for the request
-         * @returns {Promise} A promise that resolves once the contents have been written
-         * @memberof ClientInterface
-         * @example
-         *      await client.putFileContents("/dir/image.png", myImageBuffer);
-         *      // Put contents without overwriting:
-         *      await client.putFileContents("/dir/image.png", myImageBuffer, { overwrite: false });
-         */
-        putFileContents: function putFileContents(remoteFilename, data, options) {
-            const putOptions = merge(baseOptions, options || {});
-            return putFile.putFileContents(remoteFilename, data, putOptions);
-        },
-
-        /**
-         * Get the upload link
-         * Only supported for Basic authentication or unauthenticated connections.
-         * @param {String} remoteFilename The path of the remote file location
-         * @param {PutOptions=} options The options for the request
-         * @memberof ClientInterface
-         * @returns {String} A upload URL
-         */
-        getFileUploadLink: function getFileUploadLink(remoteFilename, options) {
-            var putOptions = merge(baseOptions, options || {});
-            return putFile.getFileUploadLink(remoteFilename, putOptions);
-        },
-
-        /**
-         * Stat a remote object
-         * @param {String} remotePath The path of the item
-         * @param {OptionsForAdvancedResponses=} options Options for the request
-         * @memberof ClientInterface
-         * @returns {Promise.<Object>} A promise that resolves with the stat data
-         */
-        stat: function stat(remotePath, options) {
-            const getOptions = merge(baseOptions, options || {});
-            return stats.getStat(remotePath, getOptions);
-        }
-    };
+  };
 }
 
 module.exports = {
-    createClient
+  createClient
 };
