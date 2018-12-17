c459aa32c20d671e2f11eff33f4a2125e94b9875
munirdalkaya <35899018+munirdalkaya@users.noreply.github.com>
2018-12-17 18:09:23 +0100

    Added getSearchDirectoryContents Method to retrieve dir. content recursively

--- source/interface/directoryContents.js
+++ source/interface/directoryContents.js
@@ -1,69 +1,82 @@
-const pathPosix = require("path-posix");
-const joinURL = require("url-join");
-const { merge } = require("../merge.js");
-const { handleResponseCode, processResponsePayload } = require("../response.js");
-const { normaliseHREF, normalisePath } = require("../url.js");
-const { getSingleValue, getValueForKey, parseXML, propsToStat } = require("./dav.js");
-const { encodePath, prepareRequestOptions, request } = require("../request.js");
+"use strict";
 
-function getDirectoryContents(remotePathRaw, options) {
-    // Strip the ending slash
-    const remotePath = remotePathRaw.replace(/\/$/, "");
-    // Join the URL and path for the request
-    const requestOptions = {
-        url: joinURL(options.remoteURL, encodePath(remotePath)),
+var path = require("path");
+
+var joinURL = require("url-join");
+var deepmerge = require("deepmerge");
+
+var fetch = require("../request.js").fetch;
+var responseHandlers = require("../response.js");
+var urlTools = require("../url.js");
+var davTools = require("./dav.js");
+
+var getValueForKey = davTools.getValueForKey;
+var getSingleValue = davTools.getSingleValue;
+
+/* BY ANNOVA */
+function getSearchDirectoryContents(remotePath, options) {
+    var fetchURL = joinURL(options.remoteURL, remotePath);
+    var fetchOptions = {
         method: "PROPFIND",
-        headers: {
-            Accept: "text/plain",
-            Depth: 1
-        },
-        responseType: "text"
+        headers: deepmerge({
+            Depth: "infinity"
+        }, options.headers)
     };
-    let response = null;
-    prepareRequestOptions(requestOptions, options);
-    return request(requestOptions)
-        .then(handleResponseCode)
-        .then(res => {
-            response = res;
-            return res.data;
-        })
-        .then(parseXML)
-        .then(result => getDirectoryFiles(result, options.remotePath, remotePath))
-        .then(files => processResponsePayload(response, files, options.details));
+    return fetch(fetchURL, fetchOptions).then(responseHandlers.handleResponseCode).then(function __handleResponseFormat(res) {
+        // Convert response to text
+        return res.text();
+    }).then(davTools.parseXML).then(function __handleResult(result) {
+        return getDirectoryFiles(result, options.remotePath, remotePath);
+    });
+}
+/* BY ANNOVA */
+
+function getDirectoryContents(remotePath, options) {
+    var fetchURL = joinURL(options.remoteURL, remotePath);
+    var fetchOptions = {
+        method: "PROPFIND",
+        headers: deepmerge({
+            Depth: 1
+        }, options.headers)
+    };
+    return fetch(fetchURL, fetchOptions).then(responseHandlers.handleResponseCode).then(function __handleResponseFormat(res) {
+        // Convert response to text
+        return res.text();
+    }).then(davTools.parseXML).then(function __handleResult(result) {
+        return getDirectoryFiles(result, options.remotePath, remotePath);
+    });
 }
 
 function getDirectoryFiles(result, serverBasePath, requestPath) {
-    const remoteTargetPath = pathPosix.join(serverBasePath, requestPath);
+    var remoteTargetPath = path.join(serverBasePath, requestPath);
     // Extract the response items (directory contents)
-    const multiStatus = getValueForKey("multistatus", result);
-    const responseItems = getValueForKey("response", multiStatus);
-    return (
-        responseItems
-            // Filter out the item pointing to the current directory (not needed)
-            .filter(item => {
-                let href = getSingleValue(getValueForKey("href", item));
-                href = normaliseHREF(href);
-                href = normalisePath(href);
-                return href !== serverBasePath && href !== remoteTargetPath;
-            })
-            // Map all items to a consistent output structure (results)
-            .map(item => {
-                // HREF is the file path (in full)
-                let href = getSingleValue(getValueForKey("href", item));
-                href = normaliseHREF(href);
-                // Each item should contain a stat object
-                const propStat = getSingleValue(getValueForKey("propstat", item));
-                const props = getSingleValue(getValueForKey("prop", propStat));
-                // Process the true full filename (minus the base server path)
-                const filename =
-                    serverBasePath === "/"
-                        ? normalisePath(href)
-                        : normalisePath(pathPosix.relative(serverBasePath, href));
-                return propsToStat(props, filename);
-            })
-    );
+    var multiStatus = getValueForKey("multistatus", result);
+    var responseItems = getValueForKey("response", multiStatus);
+    return responseItems
+    // Filter out the item pointing to the current directory (not needed)
+    .filter(function __filterResponseItem(item) {
+        var href = getSingleValue(getValueForKey("href", item));
+        href = urlTools.normaliseHREF(href);
+        href = urlTools.normalisePath(href);
+        return href !== serverBasePath && href !== remoteTargetPath;
+    })
+    // Map all items to a consistent output structure (results)
+    .map(function __mapResponseItem(item) {
+        // HREF is the file path (in full)
+        var href = getSingleValue(getValueForKey("href", item));
+        href = urlTools.normaliseHREF(href);
+        href = decodeURI(href);
+        href = urlTools.normalisePath(href);
+        // Each item should contain a stat object
+        var propStat = getSingleValue(getValueForKey("propstat", item));
+        var props = getSingleValue(getValueForKey("prop", propStat));
+        // Process the true full filename (minus the base server path)
+        var filename = serverBasePath === "/" ? href : urlTools.normalisePath(path.relative(serverBasePath, href));
+        return davTools.propsToStat(props, filename);
+    });
 }
 
 module.exports = {
-    getDirectoryContents
+    getSearchDirectoryContents : getSearchDirectoryContents, 
+    getDirectoryContents: getDirectoryContents
 };
